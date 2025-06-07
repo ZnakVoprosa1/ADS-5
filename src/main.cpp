bed8a9925adb79c82103b57d42444a7b2c83710b
Степан <184nikiforov7b@mail.ru>
2025-06-07 13:17:28 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
+
+std::string infx2pstfx(const std::string& inf);
+int eval(const std::string& post);
 
 int main() {
-    return 0;
+  std::string inputExpression;
+
+  if (std::getline(std::cin, inputExpression)) {
+    auto postfixForm = infx2pstfx(inputExpression);
+    std::cout << postfixForm << std::endl;
+    std::cout << eval(postfixForm) << std::endl;
+  }
+
+  return 0;
 }
