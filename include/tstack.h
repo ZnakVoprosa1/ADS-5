13f65fb0c41a2fee974d151f7dac7f60aaee286b
Степан <184nikiforov7b@mail.ru>
2025-06-07 13:17:01 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,49 +1,47 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
 
 #include <stdexcept>
 
-template<typename T, int SIZE>
+template<typename Type, int Capacity>
 class TStack {
  public:
-  TStack() : idx_(-1) {}
+  TStack() : top_(-1) {}
 
-  void add(const T& elem) {
-    if (isFull()) {
-      throw std::overflow_error("Stack overflow");
+  void add(const Type& element) {
+    if (full()) {
+      throw std::overflow_error("Stack is full");
     }
-    data_[++idx_] = elem;
+    storage_[++top_] = element;
   }
 
   void remove() {
-    if (isEmpty()) {
-      throw std::underflow_error("Stack underflow");
+    if (empty()) {
+      throw std::underflow_error("Stack is empty");
     }
-    --idx_;
+    --top_;
   }
 
-  T peek() const {
-    if (isEmpty()) {
-      throw std::underflow_error("Empty stack");
+  Type peek() const {
+    if (empty()) {
+      throw std::underflow_error("No elements in stack");
     }
-    return data_[idx_];
+    return storage_[top_];
   }
 
-  bool isEmpty() const {
-    return idx_ == -1;
+  bool empty() const {
+    return top_ == -1;
   }
 
-  bool isFull() const {
-    return idx_ + 1 >= limit;
+  bool full() const {
+    return top_ + 1 >= MaxSize;
   }
 
  private:
-  static constexpr int limit = SIZE;
-  T data_[limit];
-  int idx_;
+  static constexpr int MaxSize = Capacity;
+  Type storage_[MaxSize];
+  int top_;
 };
 
 #endif  // INCLUDE_TSTACK_H_
